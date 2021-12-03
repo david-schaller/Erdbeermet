39779150be6da2e0bee81b09509285d2f9700fd1
David Schaller <38377210+david-schaller@users.noreply.github.com>
2021-12-03 13:53:59 +0100

    restructuring

--- src/erdbeermet/simulation.py
+++ src/erdbeermet/simulation.py
@@ -8,13 +8,13 @@ import erdbeermet.tools.FileIO as FileIO
 
 class Scenario:
     
-    def __init__(self, N, circular):
+    def __init__(self, N, history, circular):
         
         self.N = N
+        self.history = history
         self.circular = circular
         
-        self.history = []
-        self.D = np.zeros((self.N, self.N))
+        self._build_matrix()
         
     
     def distances(self):
@@ -28,6 +28,9 @@ class Scenario:
     
     
     def get_circular_ordering(self):
+        
+        if not self.circular:
+            return False
         
         visited = {0}
         ordering = [0]
@@ -53,23 +56,79 @@ class Scenario:
             
             delta_str = '[' + ','.join(str(d) for d in delta) + ']'
             print(f"({x}, {y}: {z}) {alpha}; {delta_str}")
+    
+    
+    def _build_matrix(self):
+        
+        self.D = np.zeros((self.N, self.N))
+        D = self.D
+    
+        if self.circular:
+            self.L = nx.DiGraph()
+            L = self.L
+            L.add_node(0)
+            L.add_edge(0, 0)
+            
+        for x, y, z, alpha, delta in self.history:
+            
+            # simple duplication event
+            if (x == y or
+                (x is None) or (y is None) or
+                alpha == 1.0 or alpha == 0.0):
+                
+                if x is None or alpha == 0.0:
+                    x = y
+                    
+                D[x, z] = 0.0
+                D[z, x] = 0.0
+                
+                for u in range(z):
+                    D[u, z] = D[x, z]
+                    D[z, u] = D[x, z]
+                    
+                if self.circular:
+                    L.remove_edge(x, y)
+                    L.add_edge(x, z)
+                    L.add_edge(z, y)
+                    
+            # recombination event      
+            else:
+                if self.circular:
+                    if not L.has_edge(x, y):
+                        raise RuntimeError(f'{x} and {y} are not neighbors')
+                        
+                    L.remove_edge(x, y)
+                    L.add_edge(x, z)
+                    L.add_edge(z, y)
+                
+                for u in range(z):
+                    if u != x and u != y:
+                        
+                        d = alpha * D[x, u] + (1 - alpha) * D[y, u]
+                        D[u, z] = d
+                        D[z, u] = d
+                        
+                D[z, x] = (1 - alpha) * D[x, y]
+                D[x, z] = (1 - alpha) * D[x, y]
+                D[z, y] = alpha * D[x, y]
+                D[y, z] = alpha * D[x, y]
+            
+            # distance increment, i.e., independent evolution after event
+            if len(delta) != z + 1:
+                raise RuntimeError(f'invalid length of delta array for z={z}')
+                        
+            for p in range(z):
+                for q in range(p+1, z+1):
+                    D[p, q] += delta[p] + delta[q]
+                    D[q, p] = D[p, q]
 
 
-def simulate(N, branching_prob=0.0, circular=False, clocklike=False):
+def random_history(N, branching_prob=0.0, circular=False, clocklike=False):
     
-    if circular and branching_prob > 0.0:
-        raise ValueError('pure duplication events are not allowed for '\
-                         'circular type R metrics')
-    
-    scenario = Scenario(N, circular)
-    scenario.branching_prob = branching_prob
-    scenario.clocklike = clocklike
-    
-    D = scenario.D
+    history = []
     
     if circular:
-        scenario.L = nx.DiGraph()
-        L = scenario.L
+        L = nx.DiGraph()
         L.add_node(0)
         L.add_edge(0, 0)
     
@@ -80,12 +139,6 @@ def simulate(N, branching_prob=0.0, circular=False, clocklike=False):
             
             x = np.random.randint(z)
             y, alpha = x, 1.0           # for the history
-            D[x, z] = 0.0
-            D[z, x] = 0.0
-            
-            for u in range(z):
-                D[u, z] = D[x, z]
-                D[z, u] = D[x, z]
                 
             if circular:
                 y = next(L.successors(x))
@@ -106,35 +159,30 @@ def simulate(N, branching_prob=0.0, circular=False, clocklike=False):
                 L.add_edge(z, y)
                 
             alpha = np.random.random()
-            
-            for u in range(z):
-                if u != x and u != y:
-                    
-                    d = alpha * D[x, u] + (1 - alpha) * D[y, u]
-                    D[u, z] = d
-                    D[z, u] = d
-                    
-            D[z, x] = (1 - alpha) * D[x, y]
-            D[x, z] = (1 - alpha) * D[x, y]
-            D[z, y] = alpha * D[x, y]
-            D[y, z] = alpha * D[x, y]
         
         # distance increment, i.e., independent evolution after event
         if not clocklike:
             delta = np.random.exponential(scale=1/N, size=z+1)
         else:
             delta = np.random.exponential(scale=1/N) * np.ones((z+1,))
-        
-        for p in range(z):
-            for q in range(p+1, z+1):
                 
-                D[p, q] += delta[p] + delta[q]
-                D[q, p] = D[p, q]
-                
-        scenario.history.append( (x, y, z, alpha, delta) )
+        history.append( (x, y, z, alpha, delta) )
     
-    return scenario
+    return history
+
+
+def simulate(N, branching_prob=0.0, circular=False, clocklike=False):
     
+    if circular and branching_prob > 0.0:
+        raise ValueError('pure duplication events are not allowed for '\
+                         'circular type R metrics')
+    
+    return Scenario(N,
+                    random_history(N, branching_prob=branching_prob,
+                                   circular=circular,
+                                   clocklike=clocklike),
+                    circular)
+
 
 def scenario_from_history(history, circular=False, stop_after=False):
         
@@ -145,75 +193,7 @@ def scenario_from_history(history, circular=False, stop_after=False):
     else:
         raise RuntimeError(f'not enough events to simulate {stop_after} items')
     
-    scenario = Scenario(N, circular)
-    
-    D = scenario.D
-    
-    if circular:
-        scenario.L = nx.DiGraph()
-        L = scenario.L
-        L.add_node(0)
-        L.add_edge(0, 0)
-        
-    for i in range(N-1):
-        x, y, z, alpha, delta = history[i]
-        
-        # simple duplication event
-        if (x == y or
-            (x is None) or (y is None) or
-            alpha == 1.0 or alpha == 0.0):
-            
-            if x is None or alpha == 0.0:
-                x = y
-                
-            D[x, z] = 0.0
-            D[z, x] = 0.0
-            
-            for u in range(z):
-                D[u, z] = D[x, z]
-                D[z, u] = D[x, z]
-                
-            if circular:
-                L.remove_edge(x, y)
-                L.add_edge(x, z)
-                L.add_edge(z, y)
-                
-        # recombination event      
-        else:
-            if circular:
-                print(history[i])
-                if not L.has_edge(x, y):
-                    raise RuntimeError(f"'{x}' and '{y}' are not neighbors!")
-                    
-                L.remove_edge(x, y)
-                L.add_edge(x, z)
-                L.add_edge(z, y)
-            
-            for u in range(z):
-                if u != x and u != y:
-                    
-                    d = alpha * D[x, u] + (1 - alpha) * D[y, u]
-                    D[u, z] = d
-                    D[z, u] = d
-                    
-            D[z, x] = (1 - alpha) * D[x, y]
-            D[x, z] = (1 - alpha) * D[x, y]
-            D[z, y] = alpha * D[x, y]
-            D[y, z] = alpha * D[x, y]
-        
-        # distance increment, i.e., independent evolution after event
-        if len(delta) != z + 1:
-            raise RuntimeError(f'invalid length of delta array for z={z}')
-                    
-        for p in range(z):
-            for q in range(p+1, z+1):
-                
-                D[p, q] += delta[p] + delta[q]
-                D[q, p] = D[p, q]
-        
-        scenario.history.append( (x, y, z, alpha, delta) )
-    
-    return scenario
+    return Scenario(N, history[:N-1], circular)
 
 
 def load(filename, circular=False, stop_after=False):
