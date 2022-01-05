f63b47e933772f6e790a32fe43e1742bea2f9217
David Schaller <38377210+david-schaller@users.noreply.github.com>
2022-01-05 11:33:04 +0100

    generate dead ends example

---
+++ examples/generate_dead_end.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from erdbeermet.simulation import simulate
+from erdbeermet.recognition import recognize
+
+
+counter = 1
+
+while True:
+    
+    print(counter)
+    
+    scenario = simulate(6)
+    
+    recognition_tree = recognize(scenario.D, print_info=False,
+                                 first_candidate_only=True)
+    
+    if recognition_tree.valid_ways == 0:
+        print(scenario.D)
+    
+        print('\n')
+        print(recognition_tree.to_newick())
+        print('\n# successes:', recognition_tree.successes)
+    
+        # visulize the recognition
+        recognition_tree.visualize(decimal_prec=5, save_as=False)
+        
+        break
+    
+    counter += 1
